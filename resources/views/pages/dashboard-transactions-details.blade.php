b046a0c81537db0e262bc88ffe53ff91653af582
Dhoni <55375925+MuchRomadhoni@users.noreply.github.com>
2023-12-23 00:17:41 +0700

    create setting controller and pages

--- resources/views/pages/dashboard-transactions-details.blade.php
+++ resources/views/pages/dashboard-transactions-details.blade.php
@@ -1,123 +1,140 @@
 @extends('layouts.dashboard')
 
 @section('title')
-    Store Dashboard Transaction
+    Store Dashboard Transaction Detail
 @endsection
 
 @section('content')
     <div class="section-content section-dashboard-home" data-aos="fade-up">
         <div class="container-fluid">
             <div class="dashboard-heading">
-                <h2 class="dashboard-title">Transactions</h2>
+                <h2 class="dashboard-title">#STORE0839</h2>
                 <p class="dashboard-subtitle">
-                    Big result start from the small one
+                    Transaction Details
                 </p>
             </div>
-            <div class="dashboard-content">
-                <ul class="nav nav-pills" id="myTab" role="tablist">
-                    <li class="nav-item" role="presentation">
-                        <a class="nav-link active" id="sell-tab" data-toggle="tab" href="#sell" role="tab"
-                            aria-controls="sell" aria-selected="true">Sell Product</a>
-                    </li>
-                    <li class="nav-item" role="presentation">
-                        <a class="nav-link" id="buy-tab" data-toggle="tab" href="#buy" role="tab"
-                            aria-controls="buy" aria-selected="false">Buy Product</a>
-                    </li>
-                </ul>
-                <div class="tab-content" id="myTabContent">
-                    <div class="tab-pane fade show active" id="sell" role="tabpanel" aria-labelledby="sell-tab">
-                        <div class="row mt-3">
-                            <div class="col-12 mt-2">
-                                <a class="card card-list d-block" href="/dashboard-transactions-details.html">
-                                    <div class="card-body">
+            <div class="dashboard-content" id="transactionDetails">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-12 col-md-4">
+                                        <img src="/images/product-details-dashboard.png" alt=""
+                                            class="w-100 mb-3" />
+                                    </div>
+                                    <div class="col-12 col-md-8">
                                         <div class="row">
-                                            <div class="col-md-1">
-                                                <img src="/images/dashboard-icon-product-1.png" alt="" />
+                                            <div class="col-12 col-md-6">
+                                                <div class="product-title">Customer Name</div>
+                                                <div class="product-subtitle">Angga Risky</div>
                                             </div>
-                                            <div class="col-md-4">
-                                                Shirup Marzzan
+                                            <div class="col-12 col-md-6">
+                                                <div class="product-title">Product Name</div>
+                                                <div class="product-subtitle">
+                                                    Shirup Marzzan
+                                                </div>
                                             </div>
-                                            <div class="col-md-3">
-                                                Angga Risky
+                                            <div class="col-12 col-md-6">
+                                                <div class="product-title">
+                                                    Date of Transaction
+                                                </div>
+                                                <div class="product-subtitle">
+                                                    12 Januari, 2020
+                                                </div>
                                             </div>
-                                            <div class="col-md-3">
-                                                12 Januari, 2020
+                                            <div class="col-12 col-md-6">
+                                                <div class="product-title">Status</div>
+                                                <div class="product-subtitle text-danger">
+                                                    @{{ status }}
+                                                </div>
                                             </div>
-                                            <div class="col-md-1 d-none d-md-block">
-                                                <img src="/images/dashboard-arrow-right.svg" alt="" />
+                                            <div class="col-12 col-md-6">
+                                                <div class="product-title">Total Amount</div>
+                                                <div class="product-subtitle">$280,409</div>
+                                            </div>
+                                            <div class="col-12 col-md-6">
+                                                <div class="product-title">Mobile</div>
+                                                <div class="product-subtitle">
+                                                    +628 2020 11111
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </a>
-                                <a class="card card-list d-block" href="/dashboard-transactions-details.html">
-                                    <div class="card-body">
+                                </div>
+                                <div class="row">
+                                    <div class="col-12 mt-4">
+                                        <h5>
+                                            Shipping Informations
+                                        </h5>
                                         <div class="row">
-                                            <div class="col-md-1">
-                                                <img src="/images/dashboard-icon-product-2.png" alt="" />
+                                            <div class="col-12 col-md-6">
+                                                <div class="product-title">Address 1</div>
+                                                <div class="product-subtitle">
+                                                    Setra Duta Cemara
+                                                </div>
                                             </div>
-                                            <div class="col-md-4">
-                                                LeBrone X
+                                            <div class="col-12 col-md-6">
+                                                <div class="product-title">Address 2</div>
+                                                <div class="product-subtitle">
+                                                    Blok B2 No. 34
+                                                </div>
                                             </div>
-                                            <div class="col-md-3">
-                                                Masayoshi
+                                            <div class="col-12 col-md-6">
+                                                <div class="product-title">
+                                                    Province
+                                                </div>
+                                                <div class="product-subtitle">
+                                                    West Java
+                                                </div>
                                             </div>
-                                            <div class="col-md-3">
-                                                11 January, 2020
+                                            <div class="col-12 col-md-6">
+                                                <div class="product-title">City</div>
+                                                <div class="product-subtitle">
+                                                    Bandung
+                                                </div>
                                             </div>
-                                            <div class="col-md-1 d-none d-md-block">
-                                                <img src="/images/dashboard-arrow-right.svg" alt="" />
+                                            <div class="col-12 col-md-6">
+                                                <div class="product-title">Postal Code</div>
+                                                <div class="product-subtitle">123999</div>
+                                            </div>
+                                            <div class="col-12 col-md-6">
+                                                <div class="product-title">Country</div>
+                                                <div class="product-subtitle">
+                                                    Indonesia
+                                                </div>
+                                            </div>
+                                            <div class="col-12">
+                                                <div class="row">
+                                                    <div class="col-md-3">
+                                                        <div class="product-title">Status</div>
+                                                        <select name="status" id="status" class="form-control"
+                                                            v-model="status">
+                                                            <option value="UNPAID">Unpaid</option>
+                                                            <option value="PENDING">Pending</option>
+                                                            <option value="SHIPPING">Shipping</option>
+                                                            <option value="SUCCESS">Success</option>
+                                                        </select>
+                                                    </div>
+                                                    <template v-if="status == 'SHIPPING'">
+                                                        <div class="col-md-3">
+                                                            <div class="product-title">
+                                                                Input Resi
+                                                            </div>
+                                                            <input class="form-control" type="text" name="resi"
+                                                                id="openStoreTrue" v-model="resi" />
+                                                        </div>
+                                                        <div class="col-md-2">
+                                                            <button type="submit" class="btn btn-success btn-block mt-4">
+                                                                Update Resi
+                                                            </button>
+                                                        </div>
+                                                    </template>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </a>
-                                <a class="card card-list d-block" href="/dashboard-transactions-details.html">
-                                    <div class="card-body">
-                                        <div class="row">
-                                            <div class="col-md-1">
-                                                <img src="/images/dashboard-icon-product-3.png" alt="" />
-                                            </div>
-                                            <div class="col-md-4">
-                                                Soffa Lembutte
-                                            </div>
-                                            <div class="col-md-3">
-                                                Shayna
-                                            </div>
-                                            <div class="col-md-3">
-                                                11 January, 2020
-                                            </div>
-                                            <div class="col-md-1 d-none d-md-block">
-                                                <img src="/images/dashboard-arrow-right.svg" alt="" />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="tab-pane fade" id="buy" role="tabpanel" aria-labelledby="buy-tab">
-                        <div class="row mt-3">
-                            <div class="col-12 mt-2">
-                                <a class="card card-list d-block" href="/dashboard-transactions-details.html">
-                                    <div class="card-body">
-                                        <div class="row">
-                                            <div class="col-md-1">
-                                                <img src="/images/dashboard-icon-product-1.png" alt="" />
-                                            </div>
-                                            <div class="col-md-4">
-                                                Shirup Marzzan
-                                            </div>
-                                            <div class="col-md-3">
-                                                Angga Risky
-                                            </div>
-                                            <div class="col-md-3">
-                                                12 Januari, 2020
-                                            </div>
-                                            <div class="col-md-1 d-none d-md-block">
-                                                <img src="/images/dashboard-arrow-right.svg" alt="" />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </a>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -126,3 +143,16 @@
         </div>
     </div>
 @endsection
+
+@push('addon-script')
+    <script src="/vendor/vue/vue.js"></script>
+    <script>
+        var transactionDetails = new Vue({
+            el: "#transactionDetails",
+            data: {
+                status: "SHIPPING",
+                resi: "BDO12308012132",
+            },
+        });
+    </script>
+@endpush
--- routes/web.php
+++ routes/web.php
@@ -30,4 +30,7 @@ Route::get('/dashboard/products/{id}', [App\Http\Controllers\DashboardProductCon
 Route::get('/dashboard/transactions', [App\Http\Controllers\DashboardTransactionController::class, 'index'])->name('dashboard-transaction');
 Route::get('/dashboard/transactions/{id}', [App\Http\Controllers\DashboardTransactionController::class, 'details'])->name('dashboard-transaction-details');
 
+Route::get('/dashboard/settings', [App\Http\Controllers\DashboardSettingController::class, 'store'])->name('dashboard-settings-store');
+Route::get('/dashboard/account', [App\Http\Controllers\DashboardSettingController::class, 'account'])->name('dashboard-settings-account');
+
 Auth::routes();
