fb8d0c4dcf054ed92aa2509645bfe8da5d9e4eba
Dhoni <55375925+MuchRomadhoni@users.noreply.github.com>
2024-01-01 16:49:21 +0700

    integrasi register

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,22 +17,14 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-        'roles',
-    ];
+    protected $fillable = ['name', 'email', 'password', 'roles', 'store_name', 'categories_id', 'store_status'];
 
     /**
      * The attributes that should be hidden for serialization.
      *
      * @var array<int, string>
      */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    protected $hidden = ['password', 'remember_token'];
 
     /**
      * The attributes that should be cast.
